66d8b6fa90b20607c74271090131ca850fd3ebde
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2021-05-04 17:27:37 -0500

    server

---
+++ server/server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+const port = 8080;
+ 
+ //use the application off of express.
+ var app = express();
+ 
+ //define the route for "/"
+ app.get("/", function (request, response){
+     response.sendFile(__dirname+"/index.html");
+ });
+ 
+ app.get("/getemail", function (request, response){
+     var firstname = request.query.firstname;
+ 
+     if (firstname != "") {
+         response.send("Your email address is " + firstname + "@gullele.com");
+     } else {
+         response.send("Please provide us first name");
+     }
+ });
+ 
+ //start the server
+ app.listen(8080);
+ app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+  });
+  
+ console.log("Something awesome to happen at http://localhost:8080");
