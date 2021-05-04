381fd204b2363cd6c36b923b9505f39bb7eb9689
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2021-05-04 17:30:32 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,28 +1,25 @@
 var express = require("express");
 const port = 8080;
  
- //use the application off of express.
- var app = express();
+//use the application off of express.
+var app = express();
  
- //define the route for "/"
- app.get("/", function (request, response){
-     response.sendFile(__dirname+"/index.html");
- });
+//define the route for "/"
+app.get("/", function (request, response){
+    response.sendFile(__dirname+"/index.html");
+});
  
- app.get("/getemail", function (request, response){
-     var firstname = request.query.firstname;
+app.get("/getemail", function (request, response){
+    var firstname = request.query.firstname;
  
-     if (firstname != "") {
-         response.send("Your email address is " + firstname + "@gullele.com");
-     } else {
-         response.send("Please provide us first name");
-     }
- });
+    if (firstname != "") {
+        response.send("Your email address is " + firstname + "@gullele.com");
+    } else {
+        response.send("Please provide us first name");
+    }
+});
  
  //start the server
- app.listen(8080);
- app.listen(port, () => {
+app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
-  });
-  
- console.log("Something awesome to happen at http://localhost:8080");
+});
