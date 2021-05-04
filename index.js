656e0f442c9716831e911691699a0294cc2a7a9f
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2021-05-04 15:00:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,12 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-    res.sendFile('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/temp_data.txt');
+  res.send(`Welcome to ZHENBO's server! This is the homepage on port ${port}`);
 });
-// app.get("/", (req, res) => {
-//   res.send(`Welcome to ZHENBO's server! This is the homepage on port ${port}`);
-// });
+
+app.get("/:name", (req, res) => {
+  res.sendFile('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/temp_data.txt');
+});
 
 // app.get("/:name", (req, res) => {
 //   res.send(`Hello ${req.params.name}`);
