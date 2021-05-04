c7e01fb9dd068b2c554eef73e86fe646944bc1d8
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2021-05-04 18:44:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const path = require('path');
+const { send } = require("process");
 const router = express.Router();
 
 // const fs = require('fs')
 
-// try {
-//   const data = fs.readFileSync('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/temp_data.txt', 'utf8')
-//   console.log(data)
-// } catch (err) {
-//   console.error(err)
-// }
+try {
+  const data = fs.readFileSync('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/temp_data.txt', 'utf8')
+  console.log(data)
+} catch (err) {
+  console.error(err)
+}
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
@@ -21,7 +23,8 @@ router.get("/", (req, res) => {
 });
 
 router.get("/feeding", (req, res) => {
-  res.sendFile('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/temp_data.txt');
+  // res.sendFile('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/temp_data.txt');
+  res.send(data);
 });
 
 // app.get("/:name/:pic", (req, res) => {
