6c17781144e8cf0717eea1d1c558b30887099e18
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2021-05-04 18:07:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require('path');
+const router = express.Router();
 
 // const fs = require('fs')
 
@@ -14,15 +16,16 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
   res.sendFile(__dirname+"/template/index.html");
 });
 
-app.get("/feeding", (req, res) => {
+router.get("/feeding", (req, res) => {
   res.sendFile('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/temp_data.txt');
 });
 
-app.get("/:name/:pic", (req, res) => {
-   res.sendFile('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/lemonade_sleep.jpg');
-});
+// app.get("/:name/:pic", (req, res) => {
+//    res.sendFile('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/lemonade_sleep.jpg');
+// });
 
+app.use('/', router);
