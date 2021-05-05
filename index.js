f42be3f0eba690539d3888368ea00a4ee5d2a55b
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2021-05-04 19:10:01 -0500

    s
    s

--- index.js
+++ index.js
@@ -7,26 +7,12 @@ const router = express.Router();
 
 // const fs = require('fs')
 
-try {
-  const data = fs.readFileSync('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/temp_data.txt', 'utf8')
-  console.log(data)
-} catch (err) {
-  console.error(err)
-}
-
-// API:
-// fetch('/calories', {
-//   method: 'POST',
-//   headers: {
-//       'Content-Type': 'application/json'
-//   },
-//   body: JSON.stringify({
-//       user: {
-//           name: "John",
-//           email: "john@example.com"
-//       }
-//   })
-// });
+// try {
+//   const data = fs.readFileSync('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/temp_data.txt', 'utf8')
+//   console.log(data)
+// } catch (err) {
+//   console.error(err)
+// }
 
 // Pages:
 app.listen(port, () => {
@@ -39,21 +25,11 @@ router.get("/", (req, res) => {
 
 app.get("/feeding", (req, res) => {
   res.sendFile('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/temp_data.txt');
-  // res.send(data);
 });
 
-// app.get("/:name/:pic", (req, res) => {
-//    res.sendFile('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/lemonade_sleep.jpg');
-// });
+app.get("/lemonade", (req, res) => {
+    // res.sendFile('/home/bitnami/htdocs/Lightsail-Setup-Custom-Domain-SSL/Lightsail-Setup-Custom-Domain-SSL-main/lemonade_sleep.jpg');
+    res.sendFile(__dirname+"/template/lemonade.html");
+ });
 
 app.use('/', router);
-// Parse URL-encoded bodies (as sent by HTML forms)
-app.use(express.urlencoded());
-
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
-
-// Access the parse results as request.body
-// app.post('/calories', function(request, response){
-//     console.log(request.body.user.email);
-// });
